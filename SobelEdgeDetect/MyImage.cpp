b414199f0a5424b90288a8061a244e1b52770858
Adam Albright <aalbright425@gmail.com>
2016-09-17 00:34:53 -0400

    Peaks is having issues. May be a thresholding issue

--- SobelEdgeDetect/MyImage.cpp
+++ SobelEdgeDetect/MyImage.cpp
@@ -1,5 +1,7 @@
 
-
+// By Adam Albright 
+// Code available at https://github.com/Rehket/SobelEdgeDetect
+// The image class containe the code for retrieveing the image, finding edges, and saving out the pgm.
 #include "MyImage.h"
 #include <iostream>
 #include <fstream>
@@ -13,8 +15,14 @@ MyImage::MyImage(int _rows, int _cols)
 	//data = new pVector *[rowsX]; //C++ method for memory allocation. Needs to be fixed.
 	//for (int i = 0; i < _cols; i++) data[i] = new pVector[colsY];
 	data = (pVector**)calloc(_cols, sizeof(pVector));
-	for (int i = 0; i < _cols; i++)
+	outX = (double **)calloc(_cols, sizeof(double));
+	outY = (double **)calloc(_cols, sizeof(double));
+	for (int i = 0; i < _cols; i++) {
 		data[i] = (pVector*)calloc(_rows, sizeof(pVector));
+		outX[i] = (double *)calloc(_rows, sizeof(double));
+		outY[i] = (double *)calloc(_rows, sizeof(double));
+	}
+		
 		
 }
 
@@ -92,6 +100,251 @@ void MyImage::sobelEdge(int _threshold, int **_xM, int **_yM, int _maskRad)
 	}
 }
 
+void MyImage::cannyEdge(float _threshold, int _rad, int _sigma, int _threshLow)
+{	
+	thresholdLow = _threshLow;
+	int maskSize = (_rad * 2) + 1;
+	int centX = maskSize / 2;
+	int centY = maskSize / 2;
+	
+
+
+	double **maskX = NULL, ** maskY = NULL;
+
+	gMask(maskX, maskY, _rad, _sigma, maskSize, centX, centY);
+
+
+	cannyConvolution(data, maskX, maskY, _rad, centX, centY);
+	cannyNormalize(outX, outY, data, _rad, _threshold);
+
+	
+
+	for (int i = 0; i < rowsX; i++)
+	{
+		for (int j = 0; j < colsY; j++)
+		{
+			if (data[i][j].norm > threshold)
+				data[i][j].edge = data[i][j].norm;
+		}
+	}
+	threshold = calcThreshold(_threshold);
+
+	cannyEdgeCanditates(data, _rad);
+	findLines(threshold);
+
+
+	
+	
+
+}
+
+void MyImage::cannyEdgeCanditates(pVector **&pic, int  _rad)
+{
+	for (int i = _rad; i < rowsX - _rad; i++)
+	{
+		for (int j = _rad; j < colsY - _rad; j++)
+		{
+			if ((pic[i][j].direction <= 22 && pic[i][j].direction > 337) || (pic[i][j].direction < 157 && pic[i][j].direction <= 202))
+			{
+				//Horizontal
+				if ((pic[i][j].norm > pic[i][j - 1].norm) && (pic[i][j].norm > pic[i][j + 1].norm)) {
+					pic[i][j].peak = 255;
+
+				}
+				else
+					pic[i][j].peak = 0;
+
+			}
+			else if ((pic[i][j].direction <= 68 && pic[i][j].direction > 23) || (pic[i][j].direction < 203))
+			{
+
+				//Diagonal/
+
+				if (pic[i][j].norm > pic[i - 1][j - 1].norm && pic[i][j].norm > pic[i + 1][j + 1].norm) {
+					pic[i][j].peak = 255;
+
+				}
+				else
+					pic[i][j].peak = 0;
+
+			}
+			else if ((pic[i][j].direction <= 157 && pic[i][j].direction > 113) || (pic[i][j].direction < 292))
+			{
+
+				//Diagonal\
+
+				if (pic[i][j].norm > pic[i + 1][j - 1].norm && pic[i][j].norm > pic[i - 1][j + 1].norm) {
+					pic[i][j].peak = 255;
+
+				}
+				else
+					pic[i][j].peak = 0;
+
+			}
+			else {
+				if (pic[i+1][j].norm > pic[i][j].norm && pic[i + 1][j].norm > pic[i][j].norm )
+				{
+					pic[i][j].peak = 255;
+
+				}
+				else
+					pic[i][j].peak = 0;
+			}
+				
+		}
+	}
+}
+
+
+void MyImage::gMask(double  **&_maskX, double **&_maskY, int  _rad, int _sigma, int _size, int _centX, int _centY)
+{
+	double xVal = 0;
+	double yVal = 0;
+
+	_maskX = (double**)calloc(_size, sizeof(double));
+	_maskY = (double**)calloc(_size, sizeof(double));
+	for (int i = 0; i < _size; i++)
+	{
+		_maskX[i] = (double*)calloc(_size, sizeof(double));
+		_maskY[i] = (double*)calloc(_size, sizeof(double));
+	}
+
+
+
+	for (int i = -_rad; i <= _rad; i++) {
+		for (int j = -_rad; j <= _rad; j++) {
+			xVal = i * (exp(-1 * (pow(i, 2) + pow(i, 2)) / (2 * pow(_sigma, 2))));
+			yVal = j * (exp(-1 * (pow(j, 2) + pow(j, 2)) / (2 * pow(_sigma, 2))));
+			_maskX[i + _centX][j + _centY] = xVal;
+			_maskY[i +_centX][j + _centY] = yVal;
+		}
+	}
+
+}
+
+void MyImage::cannyConvolution( pVector **&pic, double ** _maskX, double ** _maskY, int _rad, int _centX, int _centY)
+{ 
+	double convX, convY;
+	int dir = 0;
+
+	for (int i = _rad; i < rowsX - _rad; i++) {
+		for (int j = _rad; j < colsY - _rad; j++) {
+			convX = convY = 0.0;
+			for (int k = -_rad; k <= _rad; k++) {
+				for (int l = -_rad; l <= _rad; l++) {
+					convX += pic[i + k][j + l].value * _maskX[k + _centX][l + _centY];
+					convY += pic[i + k][j + l].value * _maskY[k + _centX][l + _centY];
+				}
+			}
+
+			dir = floor(atan2(convY, convX) / 3.14159265 * 180.0) + 360;
+			pic[i - _rad][j - _rad].direction = dir % 360;
+
+			outX[i][j] = convX;
+			outY[i][j] = convY;
+		}
+	}
+}
+
+
+
+void MyImage::cannyNormalize(double ** _outX, double ** _outY, pVector **&_pic, int _rad, float _threshold)
+{	
+	
+	double temp = 0;
+	for (int i = _rad; i < rowsX - _rad; i++)
+	{
+		for (int j = _rad; j < colsY - _rad; j++)
+		{
+			temp = sqrt(pow(_outX[i][j], 2) + pow(_outY[i][j], 2));
+			_pic[i][j].intensity = temp;
+			
+			if (temp < minIVal)
+			{
+				minIVal = temp;
+			}
+			else if (temp > maxIVal) {
+				maxIVal = temp;
+				//cout << maxIVal << "\n";
+			}
+		}
+	}
+	temp = 0;
+
+	for (int i = 0; i < rowsX; i++)
+	{
+		for (int j = 0; j < colsY; j++) 
+		{
+			temp = _pic[i][j].intensity / maxIVal;
+			_pic[i][j].norm = floor(temp * 255.0);
+			histogram[_pic[i][j].norm]++;
+		}
+	}
+
+}
+
+void MyImage::findLines(float _threshold, int _sizeX, int _sizeY)
+{
+	for (int i = 1; i < _sizeX-1; i++) {
+		for (int j = 1; j < _sizeY-1; j++) {
+			if (data[i][j].peak == 255) {
+				findNeighbors(_threshold*.5, i, j);
+			}
+		}
+	}
+
+}
+
+
+void MyImage::findNeighbors(int _thresholdLow, int _x, int _y)
+{
+	//cout << "Low threshold is " << _thresholdLow <<". \n";
+	if (data[_x][_y].edge < _thresholdLow || data[_x][_y].lines == 255)
+	{ //Base Case
+		return;
+	}
+		data[_x][_y].lines = 255;
+
+		if ((data[_x][_y].direction <= 22 && data[_x][_y].direction > 337) || (data[_x][_y].direction < 157 && data[_x][_y].direction <= 202))
+		{
+			//Look Vertical
+			if (data[_x][_y + 1].edge > data[_x][_y - 1].edge && data[_x][_y + 1].edge > _thresholdLow)
+			{
+				return findNeighbors(_thresholdLow, _x, _y + 1);
+			}
+			else if (data[_x][_y - 1].edge > _thresholdLow)
+			{
+				return findNeighbors(_thresholdLow, _x, _y - 1);
+			}
+		}
+
+		else if ((data[_x][_y].direction <= 68 && data[_x][_y].direction > 23) || (data[_x][_y].direction < 203 && data[_x][_y].direction <= 248))
+		{
+
+			//Diagonal
+			if (data[_x + 1][_y + 1].edge > data[_x - 1][_y - 1].edge && data[_x + 1][_y + 1].edge > _thresholdLow)
+			{	
+				return findNeighbors(_thresholdLow, _x + 1, _y - 1);
+			}
+			else if (data[_x - 1][_y - 1].edge > _thresholdLow)
+			{
+				return findNeighbors(_thresholdLow, _x - 1, _y - 1);
+			}
+
+		}
+		else {
+			//Horizontal
+			if (data[_x + 1][_y].edge > data[_x][_y].edge && data[_x + 1][_y].edge > _thresholdLow)
+			{	
+				return findNeighbors(_thresholdLow, _x + 1, _y);
+			}
+			else if (data[_x + 1][_y].edge > _thresholdLow) {
+				return findNeighbors(_thresholdLow, _x + 1, _y);
+			}
+		}
+}
+
+
 //fillData gets the PGN image data from the specified file. The image needs to have equal width and height.
 void MyImage::fillPGM(char *_fileName, int _size)
 {	
@@ -112,20 +365,95 @@ void MyImage::fillPGM(char *_fileName, int _size)
 }
 
 
+
 //Saves the image as a PGM. Rows and cols are used to indicate the size of the image. The fileName parameter is what the file will be called.
 //The flag is iused to indicate what pata is saved into the pgm. 
-void MyImage::toPGM(int _rows, int _cols, char* _fileName, string _threshold, int _flag)
+void MyImage::toPGM16(int _rows, int _cols, char* _fileName, string _threshold)
+{
+	int     i = 0, j = 0;
+
+	string ft(".pgm");
+	string it("_intensity");
+	ofstream out;
+
+	out.open(_fileName + it + ft, ios::out | ios::binary);
+
+	//File header
+	out << "P5\n";
+	out << _rows << " " << _cols << "\n";
+	out << maxIVal << "\n"; //Color range
+
+	if (maxIVal < 255) {
+		for (i = 0; i<_rows; i++)
+		{
+			for (j = 0; j<_cols; j++)
+			{
+
+				out << char (data[i][j].intensity);
+			}
+		}
+		out.close();
+		return;
+	}
+
+	for (i = 0; i<_rows; i++)
+	{
+		for (j = 0; j<_cols; j++)
+		{
+
+			out << unsigned short(data[i][j].intensity);
+		}
+	}
+	out.close();
+	return;
+}
+
+void MyImage::toPGM16(int _rows, int _cols, char * _name, double **arr)
+{
+	int     i = 0, j = 0;
+
+	string ft(".pgm");
+
+	ofstream out;
+
+	out.open(_name + ft, ios::out | ios::binary);
+
+	//File header
+	out << "P5\n";
+	out << _rows << " " << _cols << "\n";
+	out << "65535" << "\n"; //Color range
+
+	for (i = 0; i<_rows; i++)
+	{
+		for (j = 0; j<_cols; j++)
+		{
+
+			out << unsigned short(arr[i][j]);
+		}
+	}
+	out.close();
+	return;
+}
+
+
+//Saves the image as a PGM. Rows and cols are used to indicate the size of the image. The fileName parameter is what the file will be called.
+//The flag is iused to indicate what pata is saved into the pgm. 
+void MyImage::toPGM8(char* _fileName, string _threshold, int _flag, int _rows, int _cols)
 {
 	int     i = 0, j = 0;
 	
 	string ft(".pgm");
-	string it("_intensity"), nom("_normalized");
+	string it("_intensity"), nom("_magnitude"), edge("_edge"), line("_final"), peak("_peak");
 	ofstream out;
 	
 	if (_flag == EDGE)
-		out.open(_fileName + _threshold + ft, ios::out | ios::binary);
+		out.open(_fileName + edge + ft, ios::out | ios::binary);
 	else if (_flag == NORM)
 		out.open(_fileName + nom + ft, ios::out | ios::binary);
+	else if (_flag == PEAK)
+		out.open(_fileName + peak + ft, ios::out | ios::binary);
+	else if (_flag == LINE)
+		out.open(_fileName + line + ft, ios::out | ios::binary);
 	else
 		out.open(_fileName + ft, ios::out | ios::binary);
 
@@ -140,19 +468,24 @@ void MyImage::toPGM(int _rows, int _cols, char* _fileName, string _threshold, in
 			{
 				if (_flag == 0)
 					out << (char)data[i][j].value;
-				if (_flag == 1 )
+				if (_flag == EDGE)
 					out << (char)data[i][j].edge;
 				if (_flag == IN)
 					out << (char)data[i][j].intensity;
 				if (_flag == NORM)
 					out << (char)data[i][j].norm;
+				if (_flag == PEAK)
+					out << (char)data[i][j].peak;
+				if (_flag == LINE)
+					out << (char)data[i][j].lines;
 			}
 		}
 		out.close();
 	return;
 }
 
-void MyImage::toPGM(int _rows, int _cols, char* _fileName, unsigned int **_arr)
+
+void MyImage::toPGM8(char* _fileName, unsigned int **_arr, int _rows, int _cols)
 {
 	int     i = 0, j = 0;
 
@@ -188,15 +521,15 @@ void MyImage::toText(char *_fileName)
 	{
 		for (j = 0; j < colsY; j++)
 		{
-			out << int(data[i][j].intensity);
-			out << " ";
+			out << int(data[i][j].direction);
+			out << "\t";
 		}
 		out << "\n";
 	}
 	out.close();
 }
 
-void MyImage::toText(char *_fileName, unsigned int **_ar)
+void MyImage::toText(char *_fileName, double **_ar, int _sizeX, int _sizeY)
 {
 	int i = 0, j = 0;
 
@@ -204,14 +537,48 @@ void MyImage::toText(char *_fileName, unsigned int **_ar)
 	ofstream out;
 	out.open(_fileName + txt, ios::out);
 
-	for (i = 0; i < rowsX; i++)
+	for (i = 0; i < _sizeX; i++)
 	{
-		for (j = 0; j < colsY; j++)
+		for (j = 0; j < _sizeY; j++)
 		{
-			out << _ar[i][j];
+			out << (_ar[i][j]);
 			out << " ";
 		}
 		out << "\n";
 	}
 	out.close();
+}
+
+int MyImage::calcThreshold(float _pcnt)
+{	
+	if (_pcnt > 1 || _pcnt < 0) 
+	{
+		cout << "ERROR: Threshold must be a percentage. \n";
+			return 0;
+	}
+	int total = 0;
+	for (int i = 0; i < 256; i++)
+	{
+		total += (histogram[i] * i);
+	}
+	cout << " Total is " << total << "\n";
+	
+	int target = total * _pcnt;
+	
+	cout << target << "\n";
+	total = 0;
+
+	for (int i = 255; i >= 0; i--) {
+		total += histogram[i]*i;
+		cout << " Total is " << total << "\t" << "Target is " << target <<".\n";
+		if (total >= target)
+		{	
+			cout << "Break \n" << i;
+			return (255 - i);
+
+		}
+
+	}
+
+	return 0;
 }
--- SobelEdgeDetect/main.cpp
+++ SobelEdgeDetect/main.cpp
@@ -9,45 +9,69 @@
 using namespace std;
 
 const int DIMENSIONS = 256;
-//int maskx[3][3] = { { -1,0,1 },{ -2,0,2 },{ -1,0,1 } };
-//int masky[3][3] = { { 1,2,1 },{ 0,0,0 },{ -1,-2,-1 } };
-
-
 
 //pVector **pic;	//The pointer to our image.
 
-
-
 int main(int argc, char **argv)
 {
-	string foo;
-	int i, j, p, q, mr, sum1, sum2, size = DIMENSIONS;
-	double threshold;
+	int i, j, p, q, mr, sum1, sum2, size = DIMENSIONS, option = 0;
+
 	
 	ofstream op;
 	char *thresh, *outName, *inName; // First Arg is File name. Second is output file, Third is threshold.
 	
-	//The input file fame
+	//Sobel or Canny
 	argc--; argv++;
-	inName = *argv;
-	
-	//The Output file name
-	argc--; argv++;
-	outName = *argv;
+	option = atoi(*argv);
 
-	MyImage pic = MyImage::MyImage(size, size);
-	pic.fillPGM(inName, size);
-	argc--; argv++;
-	while (argc > 0) 
-	{
-		thresh = *argv;
-		threshold = atof(thresh);
-		pic.sobelEdge(threshold, NULL, NULL, 1);
-		pic.toPGM(size, size, outName, thresh, EDGE);
+
+
+/*		//The input file fame
 		argc--; argv++;
+		inName = *argv;
+
+		//The Output file name
+		argc--; argv++;
+		outName = *argv;
+
+		MyImage pic = MyImage::MyImage(size, size);
+		pic.fillPGM(inName, size);
+		pic.toPGM8(size, size, outName, "0", 1);
+		argc--; argv++;
+		while (argc > 0)
+		{
+			thresh = *argv;
+			threshold = atof(thresh);
+			pic.sobelEdge(threshold, NULL, NULL, 1);
+			pic.toPGM8(size, size, outName, thresh, EDGE);
+			argc--; argv++;
+		}
 	}
+	*/
+
+		//The input file fame
+		argc--; argv++;
+		inName = *argv;
+
+		//The Output file name
+		argc--; argv++;
+		outName = *argv;
+
+		MyImage pic = MyImage::MyImage(size, size);
+		pic.fillPGM(inName, size);
+		pic.toPGM8(outName, "0", 0);
+		pic.cannyEdge( .50 , 1, 1);
+		pic.toPGM8(outName, "100", NORM);
+		pic.toPGM8(outName, " 150", PEAK);
+		pic.toPGM8(outName, " 150", EDGE);
+		pic.toPGM8(outName, " 150", LINE);
+		//pic.toPGM8(outName, to_string(pic.threshold), LINE);
+	
+		//cout << "Parameter not recogognized.\n Expects 0 or 1 for sobel or Canny edge dectection respectivly.";
+	
 
 
+	cin.get();
 
 
 	//pic = (pVector**)calloc(size, sizeof(pVector));
@@ -58,7 +82,6 @@ int main(int argc, char **argv)
 	
 	//pic.sobelEdge(thresholdL, NULL, NULL, 1);
 	//pic.toPGM(size, size, outName, LOW);
-
 
 	op.close();
     return 0;
